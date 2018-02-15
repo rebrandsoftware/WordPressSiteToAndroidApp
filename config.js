f23b53f0cbe64d507adbfaee6e69c97b748d7e0e
Mike Gibson <mike@rebrandsoftware.com>
2018-02-15 14:28:55 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'mikegibson-work-android',
+		wp_ws_url : 'http://mikegibson.work/wp-appkit-api/mikegibson-work-android',
+		wp_url : 'http://mikegibson.work',
+		theme : 'q-android',
+		version : '1',
+		app_title : 'MikeGibson.work',
+		app_platform : 'android',
+		gmt_offset : -5,
+		debug_mode : 'off',
+		auth_key : '0q7rnWhfnsI7AVQW9kDLBevtOA7oOpDcDStZ7OtTyn3cDQk7bs1xaZlKkLeb1RCB',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
